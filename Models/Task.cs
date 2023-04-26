04cc24725c9619f602687c0f4b9c74a85858c543
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-04-26 23:14:09 +0300

    Added a Razor for the main task and subtask, as well as models for the same main task and subtask

---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Trello.Models
+{
+    public class Task
+    {
+        public String? TaskTitle { get; set; }
+        public List<TaskItem> Items { get; set; }
+
+        public Task()
+        {
+            Items = new List<TaskItem>();
+        }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Trello.Models
+{
+    public class TaskItem
+    {
+        public String? Task { get; set; }
+        public Int32 Check { get; set; }
+        public Int32 Fixed { get; set; }
+        public Int32 Comment { get; set; }
+        public String? AvatarURL { get; set; }
+    }
+}
