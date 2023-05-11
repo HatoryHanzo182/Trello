b0c129a35148c1925316a56114637d88a5cf4172
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-05-11 18:48:56 +0300

    Small bug fix.

--- wwwroot/js/HandlerModalWindows.js
+++ wwwroot/js/HandlerModalWindows.js
@@ -1,11 +1,38 @@
-﻿var open_modal_add_task = document.getElementById("add_task");
-var close_modal_add_task = document.getElementById("cencel_window_task");
-
-if (open_modal_add_task) { open_modal_add_task.onclick = ShowMoadalAddTask; }
-if (close_modal_add_task) { close_modal_add_task.onclick = CloseModalAddTask; }
-
-$(document).ready(function ()
+﻿$(document).ready(function ()
 {
+    $("#add_task").click(function () {
+        console.log("OPEN MODAL WINDOW FOR ADD TASK");
+        $("#MoadlTask").css("display", "block");
+    });
+
+    $("#cencel_window_task").click(function () {
+        console.log("CLOSE MODAL WINDOW FOR ADD TASK");
+        $("#MoadlTask").css("display", "none");
+        $("#MoadlTask input").val("");
+    });
+
+    $(document).ready(function () {
+        $('body').on('click', '.add-button-task', function () {
+            var task = { TaskTitle: $('#taskNameInput').val() };
+
+            $.ajax
+                ({
+                    url: '/Board/AddNewTask',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify(task),
+                }).done(function () {
+                    $('#main').load('/Board/Board #main', function () {
+                        $('.add-task-item-btn').on('click', function () { $('#ModalTaskItem_' + $(this).data('task-id')).show(); });
+                    });
+                });
+
+            CloseModalAddTask();
+        });
+    });
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////
+
     $('.add-task-item-btn').on('click', function ()
     {
         console.log("OPEN MODAL WINDOW FOR ADD ITEM TASK");
@@ -13,14 +40,20 @@ $(document).ready(function ()
         $('#ModalTaskItem_' + $(this).data('task-id')).show();
     });
 
-    $('.cancel-button').on('click', function ()
+    $(document).on('click', '.cancel-button', function ()
     {
         console.log("CLOSE MODAL WINDOW FOR ADD ITEM TASK");
 
         $(this).closest('.modal').hide();
     });
 
-    $('.add-button').on('click', function ()
+    $(document).on('click', '.task-avatar img', function () {
+        $('.task-avatar img').not(this).removeClass('rounded-image');
+        $(this).addClass('rounded-image');
+        $('.add-button').data('avatarURL', $(this).attr('src'));
+    });
+
+    $(document).on('click', '.add-button', function ()
     {
         var modal = $(this).closest('.modal');
 
@@ -38,58 +71,15 @@ $(document).ready(function ()
             url: '/Board/AddNewTaskItem',
             method: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify(item),
-            success: function ()
+            data: JSON.stringify(item)
+        }).done(function ()
+        {
+            $('#main').load('/Board/Board #main', function ()
             {
-                $('#main').load('/Board/Board #main');
-
-                modal.hide();
-            }
-        });
-    });
-});
-
-function ShowMoadalAddTask()
-{
-    var add_modal = document.getElementById("MoadlTask");
-
-    console.log("OPEN MODAL WINDOW FOR ADD TASK");
-
-    if (add_modal) { add_modal.style.display = "block"; }
-}
-function CloseModalAddTask()
-{
-    var close_modal_add_task = document.getElementById("MoadlTask");
-    var task = close_modal_add_task.getElementsByTagName("input")[0];
-
-    task.value = "";
-
-    console.log("CLOSE MODAL WINDOW FOR ADD TASK");
-
-    if (close_modal_add_task) { close_modal_add_task.style.display = "none"; }
-}
-
-$('.task-avatar img').click(function ()
-{
-    $('.task-avatar img').not(this).removeClass('rounded-image');
-    $(this).addClass('rounded-image');
-    $('.add-button').data('avatarURL', $(this).attr('src'));
-});
-$(document).ready(function ()
-{
-    $('body').on('click', '.add-button-task', function ()
-    {
-        var task = { TaskTitle: $('#taskNameInput').val() };
-
-        $.ajax
-            ({
-                url: '/Board/AddNewTask',
-                method: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(task),
-                success: function () { $('#main').load('/Board/Board #main'); }
+                $('.add-task-item-btn').on('click', function () { $('#ModalTaskItem_' + $(this).data('task-id')).show(); });
             });
 
-        CloseModalAddTask();
+            modal.hide();
+        });
     });
 });
