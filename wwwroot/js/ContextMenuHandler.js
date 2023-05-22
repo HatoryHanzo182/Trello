f43e0995822c00933f476f2f3d56cfa70f607385
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-05-22 23:22:11 +0300

    in this version, we created and styled the context menu in a peculiar way, it remains to fix a couple of bugs and you can go to the logical part of the context menu

--- wwwroot/js/ContextMenuHandler.js
+++ wwwroot/js/ContextMenuHandler.js
@@ -1,1 +1,15 @@
-﻿
+﻿$('.task-title').on('contextmenu', function (event)
+{
+    event.preventDefault();
+
+    var posX = event.pageX - 30;
+    var posY = event.pageY - 80;
+
+    $('#contextMenu').css({ top: posY, left: posX }).show();
+
+    $(document).on('click', function (event)
+    {
+        if (!$(event.target).closest('#contextMenu').length)
+            $('#contextMenu').hide();
+    });
+});
